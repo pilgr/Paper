d8e7c2bcd1165985fa1cce5fafc0f5158b672ab8
pilgr <aleksey.masny@gmail.com>
2015-06-26 01:18:44 +0300

    Improve benchmark

--- paperdb/src/androidTest/java/io/paperdb/benchmark/Benchmark.java
+++ paperdb/src/androidTest/java/io/paperdb/benchmark/Benchmark.java
@@ -1,8 +1,10 @@
 package io.paperdb.benchmark;
 
+import android.os.SystemClock;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
 
 import com.orhanobut.hawk.Hawk;
 
@@ -14,53 +16,126 @@ import java.util.List;
 import io.paperdb.Paper;
 import io.paperdb.testdata.Person;
 import io.paperdb.testdata.TestDataGenerator;
-import util.DelayMeasurer;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 
-// z3c
-//        06-05 21:47:05.199    1673-1692/io.paperdb.test D/DelayMeasurer﹕ Paper read/writes/10 102ms
-//        06-05 21:47:06.856    1673-1692/io.paperdb.test D/DelayMeasurer﹕ Hawk read/writes/10 165ms
-//Genymotion lollipop 100
-//        06-08 14:24:18.627    3080-3100/io.paperdb.test D/DelayMeasurer﹕ Paper read/writes/100 18ms
-//        06-08 14:24:21.880    3080-3100/io.paperdb.test D/DelayMeasurer﹕ Hawk read/writes/100 32ms
-
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class Benchmark extends AndroidTestCase{
+public class Benchmark extends AndroidTestCase {
 
-    private static final String BENCHMARK_DATA = "benchmark-data";
+    private static final String TAG = "paper-benchmark";
+
     @SuppressWarnings("FieldCanBeLocal")
-    private static final int REPEAT_COUNT = 100;
+    private static final int REPEAT_COUNT = 30;
 
     @Test
-    public void testReadWrite100Items() throws Exception {
+    public void testReadWrite500Contacts() throws Exception {
         final List<Person> contacts = TestDataGenerator.genPersonList(500);
+        Paper.clear(getTargetContext());
+        Paper.init(getTargetContext());
+        long paperTime = runTest(new PaperReadWriteContactsTest(), contacts, REPEAT_COUNT);
 
-        DelayMeasurer.start("Paper read/writes");
-        paperDbReadWrite(contacts, REPEAT_COUNT);
-        DelayMeasurer.finish("Paper read/writes", REPEAT_COUNT);
-
-        DelayMeasurer.start("Hawk read/writes");
-        hawkReadWrite(contacts, REPEAT_COUNT);
-        DelayMeasurer.finish("Hawk read/writes", REPEAT_COUNT);
-    }
-
-    private void hawkReadWrite(List data, int repeat) {
         Hawk.init(getTargetContext());
         Hawk.clear();
+        long hawkTime = runTest(new HawkReadWriteContactsTest(), contacts, REPEAT_COUNT);
+
+        printResults("Read/write 500 contacts", paperTime, hawkTime);
+    }
+
+    @Test
+    public void testWrite500Contacts() throws Exception {
+        final List<Person> contacts = TestDataGenerator.genPersonList(500);
+        Paper.clear(getTargetContext());
+        Paper.init(getTargetContext());
+        long paperTime = runTest(new PaperWriteContactsTest(), contacts, REPEAT_COUNT);
+
+        Hawk.init(getTargetContext());
+        Hawk.clear();
+        long hawkTime = runTest(new HawkWriteContactsTest(), contacts, REPEAT_COUNT);
+
+        printResults("Write 500 contacts", paperTime, hawkTime);
+    }
+
+    @Test
+    public void testRead500Contacts() throws Exception {
+        final List<Person> contacts = TestDataGenerator.genPersonList(500);
+        Paper.clear(getTargetContext());
+        Paper.init(getTargetContext());
+        runTest(new PaperWriteContactsTest(), contacts, REPEAT_COUNT); //Prepare
+        long paperTime = runTest(new PaperReadContactsTest(), contacts, REPEAT_COUNT);
+
+        Hawk.init(getTargetContext());
+        Hawk.clear();
+        runTest(new HawkWriteContactsTest(), contacts, REPEAT_COUNT); //Prepare
+        long hawkTime = runTest(new HawkReadContactsTest(), contacts, REPEAT_COUNT);
+
+        printResults("Read 500 contacts", paperTime, hawkTime);
+    }
+
+    private void printResults(String name, long paperTime, long hawkTime) {
+        Log.i(TAG, String.format("..................................\n%s \n Paper: %d \n Hawk: %d",
+                name, paperTime, hawkTime));
+    }
+
+    private <T> long runTest(TestTask<T> task, T extra, int repeat) {
+        long start = SystemClock.uptimeMillis();
         for (int i = 0; i < repeat; i++) {
-            Hawk.put(BENCHMARK_DATA, data);
-            Hawk.get(BENCHMARK_DATA);
+            task.run(i, extra);
+        }
+        return (SystemClock.uptimeMillis() - start) / repeat;
+    }
+
+    interface TestTask<T> {
+        void run(int i, T extra);
+    }
+
+    private class PaperReadWriteContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Paper.put(key, extra);
+            Paper.<List<Person>>get(key);
         }
     }
 
-    private void paperDbReadWrite(List data, int repeat) {
-        Paper.clear(getTargetContext());
-        Paper.init(getTargetContext());
-        for (int i = 0; i < repeat; i++) {
-            Paper.put(BENCHMARK_DATA, data);
-            Paper.get(BENCHMARK_DATA);
+    private class HawkReadWriteContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Hawk.put(key, extra);
+            Hawk.<List<Person>>get(key);
+        }
+    }
+
+    private class PaperWriteContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Paper.put(key, extra);
+        }
+    }
+
+    private class HawkWriteContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Hawk.put(key, extra);
+        }
+    }
+
+    private class PaperReadContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Paper.<List<Person>>get(key);
+        }
+    }
+
+    private class HawkReadContactsTest implements TestTask<List<Person>> {
+        @Override
+        public void run(int i, List<Person> extra) {
+            String key = "contacts" + i;
+            Hawk.<List<Person>>get(key);
         }
     }
 }
