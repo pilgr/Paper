e9e129b08ad94a1520eccfb87084bd677e4de972
Wellington Guimaraes <well@opinibox.com>
2016-01-17 23:11:04 -0300

    Oops! The UUID import was missing!

--- paperdb/src/main/java/io/paperdb/DbStoragePlainFile.java
+++ paperdb/src/main/java/io/paperdb/DbStoragePlainFile.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import de.javakaffee.kryoserializers.ArraysAsListSerializer;
 import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
