67e35d52ba29d84984cf09bd0780edeab8ac82fb
Wellington Guimaraes <wellguimaraes@gmail.com>
2016-01-18 00:27:13 -0300

    Trying to pass on TravisCI test

--- paperdb/src/androidTest/java/io/paperdb/PaperTest.java
+++ paperdb/src/androidTest/java/io/paperdb/PaperTest.java
@@ -25,7 +25,6 @@ public class PaperTest {
     @Before
     public void setUp() throws Exception {
         Paper.init(getTargetContext());
-        Paper.addSerializer(DateTime.class, new JodaDateTimeSerializer());
         Paper.book().destroy();
     }
 
@@ -175,8 +174,10 @@ public class PaperTest {
 
     @Test
     public void testCustomSerializer() {
+        Paper.addSerializer(DateTime.class, new JodaDateTimeSerializer());
+
         DateTime now = DateTime.now(DateTimeZone.UTC);
-        Paper.book().write("joda-datetime", now);
-        assertEquals(Paper.book().read("joda-datetime"), now);
+        Paper.book("custom").write("joda-datetime", now);
+        assertEquals(Paper.book("custom").read("joda-datetime"), now);
     }
 }
