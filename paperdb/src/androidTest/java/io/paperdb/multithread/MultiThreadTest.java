54bc1267350dd1fa172c772cec32c2dff7c48c36
pilgr <aleksey.masny@gmail.com>
2017-09-23 19:50:34 +0300

    Improve multi thread tests

--- paperdb/src/androidTest/java/io/paperdb/multithread/MultiThreadTest.java
+++ paperdb/src/androidTest/java/io/paperdb/multithread/MultiThreadTest.java
@@ -1,5 +1,6 @@
 package io.paperdb.multithread;
 
+import android.support.annotation.NonNull;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
@@ -23,7 +24,9 @@ import io.paperdb.testdata.Person;
 import io.paperdb.testdata.TestDataGenerator;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests read/write into Paper data from multiple threads
@@ -40,66 +43,54 @@ public class MultiThreadTest {
     }
 
     @Test
-    public void synchronizedBySameKey() throws InterruptedException {
-        final List<Person> dataset = TestDataGenerator.genPersonList(10000);
-        final CountDownLatch allTasksLatch = new CountDownLatch(2);
-        final CountDownLatch writeStartLatch = new CountDownLatch(1);
+    public void read_write_same_key() throws InterruptedException {
+        startWritingLargeDataSetInSeparateThread("dataset");
 
-        final Thread readThread = new Thread() {
-            @Override
-            public void run() {
-                Log.d(TAG, "readRunnable: start");
-                // Read for same key 'dataset' should be postponed until writing is done
-                List<Person> readData = Paper.book().read("dataset", Collections.<Person>emptyList());
-                assertEquals(10000, readData.size());
-                Log.d(TAG, "readRunnable: finish");
-                allTasksLatch.countDown();
-            }
-        };
-
-        Thread writeThread = new Thread() {
-            @Override
-            public void run() {
-                readThread.start();
-
-                writeStartLatch.countDown();
-                Log.d(TAG, "writeRunnable: start");
-                Paper.book().write("dataset", dataset);
-                Log.d(TAG, "writeRunnable: finish");
-                allTasksLatch.countDown();
-            }
-        };
-
-        writeThread.start();
-        allTasksLatch.await(10000, TimeUnit.SECONDS);
+        Log.d(TAG, "read dataset: start");
+        // Read for same key 'dataset' should be postponed until writing is done
+        List<Person> readData = Paper.book().read("dataset", Collections.<Person>emptyList());
+        assertEquals(10000, readData.size());
+        Log.d(TAG, "read dataset: finish");
     }
 
     @Test
-    public void notSynchronizedByDifferentKeys() throws InterruptedException {
-        final List<Person> dataset = TestDataGenerator.genPersonList(10000);
-        final CountDownLatch writeStartLatch = new CountDownLatch(1);
-        final CountDownLatch writeEndLatch = new CountDownLatch(1);
+    public void write_exists_same_key() throws InterruptedException {
+        assertFalse(Paper.book().exist("dataset"));
 
+        startWritingLargeDataSetInSeparateThread("dataset");
+
+        Log.d(TAG, "check dataset exists: start");
+        // Read for same key 'dataset' should be postponed until writing is done
+        assertTrue(Paper.book().exist("dataset"));
+        Log.d(TAG, "check dataset exists: finish");
+    }
+
+    @Test
+    public void write_delete_same_key() throws InterruptedException {
+        assertFalse(Paper.book().exist("dataset"));
+
+        startWritingLargeDataSetInSeparateThread("dataset");
+
+        Log.d(TAG, "check dataset delete: start");
+        // Read for same key 'dataset' should be postponed until writing is done
+        Paper.book().delete("dataset");
+        assertFalse(Paper.book().exist("dataset"));
+        Log.d(TAG, "check dataset delete: finish");
+    }
+
+    @Test
+    public void read_write_different_keys() throws InterruptedException {
         // Primary write something else
         Paper.book().write("city", "Victoria");
+
         // Start writing large dataset
-        new Thread() {
-            @Override
-            public void run() {
-                writeStartLatch.countDown();
-                Log.d(TAG, "write dataset: start");
-                Paper.book().write("dataset", dataset);
-                Log.d(TAG, "write dataset: finish");
-                writeEndLatch.countDown();
-            }
-        }.start();
-        writeStartLatch.await(10000, TimeUnit.SECONDS);
+        CountDownLatch writeEndLatch = startWritingLargeDataSetInSeparateThread("dataset");
 
         Log.d(TAG, "read other key: start");
         // Read for different key 'city' should be locked by writing other key 'dataset'
         assertEquals("Victoria", Paper.book().read("city"));
         assertEquals(1, writeEndLatch.getCount());
-        writeEndLatch.await();
+        writeEndLatch.await(5, TimeUnit.SECONDS);
         assertEquals(10000, Paper.book().<List<Person>>read("dataset").size());
         Log.d(TAG, "read other key: finish");
     }
@@ -122,6 +113,29 @@ public class MultiThreadTest {
         for (Future<Object> future : futures) {
             future.get();
         }
+    }
+
+    @NonNull
+    private CountDownLatch startWritingLargeDataSetInSeparateThread(
+            @SuppressWarnings("SameParameterValue") final String key) throws InterruptedException {
+        final CountDownLatch writeStartLatch = new CountDownLatch(1);
+        final CountDownLatch writeFinishLatch = new CountDownLatch(1);
+        new Thread() {
+            final List<Person> dataset = TestDataGenerator.genPersonList(10000);
+
+            @Override
+            public void run() {
+                Log.d(TAG, "write '" + key + "': start");
+                writeStartLatch.countDown();
+                Paper.book().write(key, dataset);
+                Log.d(TAG, "write '" + key + "': finish");
+                writeFinishLatch.countDown();
+            }
+        }.start();
+        writeStartLatch.await(5, TimeUnit.SECONDS);
+        // A small delay is required to let writer thread start writing data and acquire a lock
+        Thread.sleep(100);
+        return writeFinishLatch;
     }
 
     private Runnable getInsertRunnable() {
