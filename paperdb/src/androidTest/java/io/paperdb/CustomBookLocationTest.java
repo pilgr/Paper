894ac9c5ae02108e3c89fc7bf7ee5320e55f6ed3
pilgr <aleksey.masny@gmail.com>
2017-10-13 22:23:28 +0700

    Add test to make sure subfolders for custom book location created recursively

--- paperdb/src/androidTest/java/io/paperdb/CustomBookLocationTest.java
+++ paperdb/src/androidTest/java/io/paperdb/CustomBookLocationTest.java
@@ -8,12 +8,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
 import java.util.HashSet;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class CustomBookLocationTest {
@@ -24,8 +26,38 @@ public class CustomBookLocationTest {
     }
 
     @Test
-    public void readWriteDelete_customLocation_defaultBook() {
+    public void readWriteDelete_customLocation_with_sub_dirs() {
+        //noinspection ResultOfMethodCallIgnored
+        deleteRecursive(new File(getTargetContext().getFilesDir() + "/custom"));
+
         String customLocation = getTargetContext().getFilesDir() + "/custom/location";
+        Book book = Paper.bookOn(customLocation);
+
+        book.write("city", "Victoria");
+        assertEquals("Victoria", book.read("city"));
+
+        // Check sub folders created recursively
+        File customSubDir = new File(getTargetContext().getFilesDir() + "/custom");
+        assertTrue(customSubDir.exists());
+        assertTrue(customSubDir.isDirectory());
+        assertEquals(1, customSubDir.listFiles().length);
+        assertTrue(customSubDir.listFiles()[0].isDirectory());
+        assertEquals("location", customSubDir.listFiles()[0].getName());
+
+        File locationSubDir = new File(getTargetContext().getFilesDir() + "/custom/location");
+        assertTrue(locationSubDir.exists());
+        assertTrue(locationSubDir.isDirectory());
+        assertEquals(1, locationSubDir.listFiles().length);
+        assertTrue(locationSubDir.listFiles()[0].isDirectory());
+        assertEquals("io.paperdb", locationSubDir.listFiles()[0].getName());
+
+        book.delete("city");
+        assertFalse(book.contains("city"));
+    }
+
+    @Test
+    public void readWriteDelete_customLocation_defaultBook() {
+        String customLocation = getTargetContext().getFilesDir() + "/custom_location";
         Book bookOnSdcard = Paper.bookOn(customLocation);
         Book defaultBook = Paper.book();
 
@@ -117,5 +149,14 @@ public class CustomBookLocationTest {
                 Paper.bookOn("/sdcard", "encyclopedia").getPath());
         assertEquals("/sdcard/encyclopedia",
                 Paper.bookOn("/sdcard/", "encyclopedia").getPath());
+    }
+
+    private void deleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory())
+            for (File child : fileOrDirectory.listFiles())
+                deleteRecursive(child);
+
+        //noinspection ResultOfMethodCallIgnored
+        fileOrDirectory.delete();
     }
 }
